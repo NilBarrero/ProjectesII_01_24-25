4a4f8a48ee21e3621288ddea5fc64b239f6b0f89
NilBarrero <159423301+NilBarrero@users.noreply.github.com>
2024-11-22 13:17:29 +0100

    Chenge Game in order

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/Change Game.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/Change Game.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeGame : MonoBehaviour
 {
-    public float delay = 5f; // Tiempo de espera antes de cambiar la escena.
-    public string[] GameScene; // Lista de nombres de escenas en la carpeta.
+    public float delay = 1f; // Tiempo de espera antes de cambiar la escena.
+    public string[] GameScene;
+    static int index = 0;// Lista de nombres de escenas en la carpeta.
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class ChangeGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     IEnumerator ChangeScene()
@@ -29,12 +30,33 @@ public class ChangeGame : MonoBehaviour
         }
         else
         {
+            if (index == 1)
+            {
+                
+            }
+            string sceneToLoad = GameScene[index];
+            SceneManager.LoadScene(sceneToLoad); // Cambia a la escena especificada.
+            index++;
+
+            // Reiniciar el índice si llegamos al final de la lista.
+            if (index >= GameScene.Length)
+            {
+                Debug.Log("Se completó la lista de escenas. Reiniciando el ciclo.");
+                index = 0; // Reinicia para comenzar desde la primera escena.
+            }
+        }
+          
+
+        /*
+        else
+        {
             // Elegir una escena aleatoria de la lista.
             int randomIndex = Random.Range(0, GameScene.Length);
             string sceneToLoad = GameScene[randomIndex];
 
             SceneManager.LoadScene(sceneToLoad); // Cambia a la escena especificada.
         }
+        */
     }
 
 }
