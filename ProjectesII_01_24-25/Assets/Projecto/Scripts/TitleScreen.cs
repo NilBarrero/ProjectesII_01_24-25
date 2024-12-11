6a17ffc75d3481bfa9e43d97625810fc831d0b13
SergiNaranjo <sergi.naranjo@enti.cat>
2024-12-11 15:30:51 +0100

    Transition animation solved
    The basic one

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/TitleScreen.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/TitleScreen.cs
@@ -6,25 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private Animator animator;
-    [SerializeField] private AnimationClip animacionFinal;
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
     public void Play()
     {
-        StartCoroutine(Animation());
-    }
-    
-    IEnumerator Animation()
-    {
-        animator.SetTrigger("Iniciar");
-        yield return new WaitForSeconds(animacionFinal.length);
         SceneManager.LoadScene(1);
     }
+    
+    
     public void Exit()
     {
         Debug.Log("Saliendo...");
