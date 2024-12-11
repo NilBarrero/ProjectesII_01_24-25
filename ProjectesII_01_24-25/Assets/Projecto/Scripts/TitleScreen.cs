73cdfa373e52957ae47bac8555444c2d447db90e
SergiNaranjo <sergi.naranjo@enti.cat>
2024-12-11 13:06:21 +0100

    Transition animation started

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/TitleScreen.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/TitleScreen.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Animator animator;
+    [SerializeField] private AnimationClip animacionFinal;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Play()
     {
+        StartCoroutine(Animation());
+    }
+    
+    IEnumerator Animation()
+    {
+        animator.SetTrigger("Iniciar");
+        yield return new WaitForSeconds(animacionFinal.length);
         SceneManager.LoadScene(1);
     }
     public void Exit()
