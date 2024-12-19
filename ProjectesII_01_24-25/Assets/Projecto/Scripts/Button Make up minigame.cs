7fe8396aa44794bdc62f38d242a01dc3db12fa19
Monke <gerard.busca@enti.cat>
2024-12-19 13:11:07 +0100

    Now games have been conected

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/Button Make up minigame.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/Button Make up minigame.cs
@@ -8,18 +8,18 @@ public class ButtonMakeupminigame : MonoBehaviour
 {
     // Start is called before the first frame updateº
     public Detectifitsxory detect;
-    private string entregaPerfecta = "entregaPerfecta 3rd";
-    private string entregaMediocre = "entregaMediocre 3rd";
-    private string entregaErronea = "entregaErronea 3rd";
+    private string entregaPerfecta = "EntregaPerfecta3rd";
+    private string entregaMediocre = "entregaMediocre3rd";
+    private string entregaErronea = "entregaErronea3rd";
 
     // Update is called once per frame
     private void OnMouseDown()
     {
-        if (detect.numOfObjects <= 2)
+        if (detect.numOfObjects == 2 || detect.numOfObjects == 1 || detect.numOfObjects == 0)
         {
             SceneManager.LoadScene(entregaMediocre);
         }
-        if (detect.numOfObjects < 4)
+        else if (detect.numOfObjects == 3)
         {
             Debug.Log("There are ");
             Debug.Log(detect.numOfObjects);
