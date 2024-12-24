e9fdde28f92f56db9ab8bc093d09414461988f16
Monke <gerard.busca@enti.cat>
2024-12-24 13:48:02 +0100

    Added particles to the coin

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/Mouse Drag.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/Mouse Drag.cs
@@ -4,41 +4,54 @@ using UnityEngine;
 
 public class MouseDrag : MonoBehaviour
 {
+    public ParticleSystem particles;
     private float startPosX;
     private float startPosY;
     private bool isBeingHeld = false;
-    // Start is called before the first frame update
-
 
     private void Update()
     {
+        // Mueve el objeto solo si está siendo arrastrado
         if (isBeingHeld)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
+            Vector3 mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
             this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+
+            // Solo iniciar las partículas si no están ya activas
+            if (!particles.isPlaying)
+            {
+                particles.Play();
+            }
         }
+        else
+            particles.Stop();
     }
 
-    // Update is called once per frame
     private void OnMouseDown()
     {
+        // Detecta si el mouse ha sido presionado
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
+            Vector3 mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
             startPosX = mousePos.x - this.transform.localPosition.x;
             startPosY = mousePos.y - this.transform.localPosition.y;
 
             isBeingHeld = true;
+
+            // Inicia las partículas directamente al presionar el mouse
+            particles.Play();
         }
     }
 
     private void OnMouseUp()
     {
+        // Cuando el mouse se suelta, detener el arrastre y las partículas
         isBeingHeld = false;
+
+        // Detener las partículas de forma limpia
+        particles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 }
