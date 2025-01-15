d18dd5e76eecc828f922a836e2dff46c646db058
SergiNaranjo <sergi.naranjo@enti.cat>
2025-01-15 17:23:44 +0100

    All done here

--- ProjectesII_01_24-25/Assets/Projecto/Scripts/Kill 'hem all/GameManager counter.cs
+++ ProjectesII_01_24-25/Assets/Projecto/Scripts/Kill 'hem all/GameManager counter.cs
@@ -7,27 +7,43 @@ public class GameManagercounter : MonoBehaviour
 {
     public int clickCount = 0;
     public int maxCount = 3;
-    public string scene;
+    public string scene1;
+    public string scene2;
+    public string scene3;
     public Animator transitionAnimator;  // Referencia al Animator para la animaci�n
     public AudioSource musicSource;      // Referencia a la fuente de m�sica
     public float fadeOutDuration = 1f;   // Duraci�n del fade out de la m�sica
     public bool dontKillCertainNumOfEnemies = false;
-    public int numOfMinNumber;
+    // public int numOfMinNumber;
     public Timer timer;
+    private int numOfScene;
 
     private void Update()
-        
+
     {
-        if (dontKillCertainNumOfEnemies && clickCount >= numOfMinNumber && clickCount < maxCount && timer.tiempoRestante == 0)
+        if (timer.tiempoRestante == 0 && dontKillCertainNumOfEnemies && clickCount == 4)
         {
+
+            Debug.Log("Jose");
+            numOfScene = 0;
             StartCoroutine(TransitionToScene());
         }
 
-        
+        else if (timer.tiempoRestante == 0 && clickCount == maxCount)
+        {
+            Debug.Log("Armario");
+            numOfScene = 1;
+            StartCoroutine(TransitionToScene());
+        }
+        else if (timer.tiempoRestante == 0 && clickCount < 4)
+        {
+            Debug.Log("OOOOOOOOOOOOOOO");
+            numOfScene = 2;
+            StartCoroutine(TransitionToScene());
+        }
 
-       
+
     }
-
     public void IncrementClickCount()
     {
         clickCount++;
@@ -38,6 +54,17 @@ public class GameManagercounter : MonoBehaviour
     {
         // Inicia la animaci�n de transici�n
         transitionAnimator.SetTrigger("StartTransition"); // Aseg�rate de tener un trigger llamado "StartTransition" en tu Animator
+
+        if (numOfScene == 0)
+        // Cargar la nueva escena
+        {
+            SceneManager.LoadScene(scene1);
+            Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+        }
+        if (numOfScene == 1)
+            SceneManager.LoadScene(scene2);
+        if (numOfScene == 2)
+            SceneManager.LoadScene(scene3);
 
         // Fade out de la m�sica
         float startVolume = musicSource.volume;
@@ -53,10 +80,12 @@ public class GameManagercounter : MonoBehaviour
         // Asegura que el volumen final sea 0
         musicSource.volume = 0f;
 
+
+
         // Espera el tiempo de la animaci�n antes de cambiar de escena
         yield return new WaitForSeconds(transitionAnimator.GetCurrentAnimatorStateInfo(0).length);
 
-        // Cargar la nueva escena
-        SceneManager.LoadScene(scene);
+
+
     }
 }
